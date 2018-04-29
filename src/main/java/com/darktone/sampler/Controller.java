8441ef5b7cd3403cbabe651e52c099d665ba81b5
Bill Bridges <wsbridges@gmail.com>
2018-04-29 10:50:55 -0500

    Debugging continues

--- src/main/java/com/darktone/sampler/Controller.java
+++ src/main/java/com/darktone/sampler/Controller.java
@@ -3,6 +3,7 @@ package com.darktone.sampler;
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalInput;
+import com.pi4j.io.gpio.PinPullResistance;
 import com.pi4j.io.gpio.RaspiPin;
 
 public class Controller {
@@ -14,7 +15,7 @@ public class Controller {
 		final GpioController gpio = GpioFactory.getInstance();
 
         // provision gpio pin #02 as an input pin with its internal pull down resistor enabled
-        final GpioPinDigitalInput myButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02);
+        final GpioPinDigitalInput myButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, "button 2", PinPullResistance.PULL_DOWN);
         myButton.addListener( new ButtonListener() );
         
         System.out.println(" --> GPIO PIN STATE: " + myButton.getPin() + " = "
