4b91e294ad7f52851eb88e471719f3dd58814433
Bill Bridges <wsbridges@gmail.com>
2018-04-29 20:45:47 -0500

    trying something else

--- src/main/java/com/darktone/sampler/ButtonListener.java
+++ src/main/java/com/darktone/sampler/ButtonListener.java
@@ -1,5 +1,6 @@
 package com.darktone.sampler;
 
+import com.pi4j.io.gpio.PinState;
 import com.pi4j.io.gpio.RaspiPin;
 import com.pi4j.io.gpio.event.GpioPinDigitalStateChangeEvent;
 import com.pi4j.io.gpio.event.GpioPinListenerDigital;
@@ -11,10 +12,12 @@ public class ButtonListener implements GpioPinListenerDigital {
 		try {
 			System.out.println(" --> GPIO PIN STATE CHANGE: " + event.getPin() + " = "
 	                + event.getState());
-			if( event.getPin() == RaspiPin.GPIO_01 ) {
+			if( event.getPin().toString().equals( "\"Pin 18\" <GPIO 1>") && event.getState() == PinState.LOW ) {
+				System.out.println("Playing sample 0");
 				Sampler.createInstance().playSample( 0 );
 			}
-			else if( event.getPin() == RaspiPin.GPIO_04 ) {
+			else if( event.getPin().toString().equals( "\"Pin 23\" <GPIO 4>" ) && event.getState() == PinState.LOW ) {
+				System.out.println( "Playing sample 1");
 				Sampler.createInstance().playSample( 1 );
 			}
 		}
