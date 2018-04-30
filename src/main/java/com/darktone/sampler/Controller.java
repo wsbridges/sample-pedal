144f4498bd87749a214ab1286df0c15f0ca2716d
Bill Bridges <wsbridges@gmail.com>
2018-04-29 19:23:18 -0500

    Trying Broadcom pins.

--- src/main/java/com/darktone/sampler/Controller.java
+++ src/main/java/com/darktone/sampler/Controller.java
@@ -4,6 +4,7 @@ import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
 import com.pi4j.io.gpio.GpioPinDigitalInput;
 import com.pi4j.io.gpio.PinPullResistance;
+import com.pi4j.io.gpio.RaspiBcmPin;
 import com.pi4j.io.gpio.RaspiPin;
 
 public class Controller {
@@ -15,17 +16,22 @@ public class Controller {
 		final GpioController gpio = GpioFactory.getInstance();
 
         // provision gpio pin #02 as an input pin with its internal pull down resistor enabled
-        final GpioPinDigitalInput myButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_04, "button 4", PinPullResistance.PULL_UP);
+        final GpioPinDigitalInput myButton = gpio.provisionDigitalInputPin(RaspiBcmPin.GPIO_18, "button 18", PinPullResistance.PULL_UP);
+        final GpioPinDigitalInput myButton2 = gpio.provisionDigitalInputPin(RaspiBcmPin.GPIO_23, "button 23", PinPullResistance.PULL_DOWN);
         //myButton.addListener( new ButtonListener() );
         System.out.println( "PULL UP" );
         System.out.println(" --> GPIO PIN STATE: " + myButton.getPin() + " = "
                 + myButton.getState());
+        System.out.println(" --> GPIO PIN STATE: " + myButton2.getPin() + " = "
+                + myButton2.getState());
 
         // keep program running until user aborts (CTRL-C)
         while(true) {
             Thread.sleep(500);
             System.out.println(" --> GPIO PIN STATE: " + myButton.getPin() + " = "
                     + myButton.getState());
+            System.out.println(" --> GPIO PIN STATE: " + myButton2.getPin() + " = "
+                    + myButton2.getState());
         }
 	}
 }
