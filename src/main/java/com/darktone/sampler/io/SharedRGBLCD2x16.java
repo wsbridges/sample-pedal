114ee69c550a250ed3c683e738e8dd7c9f2f36d7
Bill Bridges <wsbridges@gmail.com>
2018-06-23 11:34:42 -0500

    Changing to RGB order for constructor.

--- src/main/java/com/darktone/sampler/io/SharedRGBLCD2x16.java
+++ src/main/java/com/darktone/sampler/io/SharedRGBLCD2x16.java
@@ -4,8 +4,8 @@ import com.pi4j.io.gpio.Pin;
 
 public class SharedRGBLCD2x16 extends SharedRGBLCD {
 
-	public SharedRGBLCD2x16(Pin ePin, Pin redPin, Pin bluePin, Pin greenPin) {
-		super(ePin, redPin, bluePin, greenPin, 2, 16);
+	public SharedRGBLCD2x16(Pin ePin, Pin redPin, Pin greenPin, Pin bluePin) {
+		super(ePin, redPin, greenPin, bluePin, 2, 16);
 	}
 
 }
